4d59501c12ca4a496a65fa0dca4bcac88619230f
Paul Herman <paulstefanherman@gmail.com>
2016-02-17 14:56:08 +0000

    Test Util.starts_with

--- unittest.ml
+++ unittest.ml
@@ -1,14 +1,12 @@
 open OUnit2;;
 
-let test1 test_ctxt = assert_equal "x" "x";;
-let test2 test_ctxt = assert_equal "x" "y";;
-
-
 (* Name the test cases and group them together *)
 let suite =
-    "suite" >::: [
-        "test1" >:: test1;
-        "test2" >:: test2
+    "Util.starts_with" >::: [
+        "string shorter than pattern" >:: (fun ctx -> assert_equal (Util.starts_with "" "abc") false);
+        "string not matching pattern" >:: (fun ctx -> assert_equal (Util.starts_with "abc" "abd") false);
+        "string matching pattern same length" >:: (fun ctx -> assert_equal (Util.starts_with "abc" "ab") true);
+        "string matching shorter pattern" >:: (fun ctx -> assert_equal (Util.starts_with "abc" "abc") true)
     ];;
 
 let () = run_test_tt_main suite;;
